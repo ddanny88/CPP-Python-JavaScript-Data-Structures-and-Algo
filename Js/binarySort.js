d23a5ce91b35d4d9a778d4362ab6b8aef9b969f0
Daniel Dominguez <ddominguez20.dd@gmail.com>
2019-05-18 16:52:29 -0500

    binarySort

---
+++ Js/binarySort.js
@@ -0,0 +1,15 @@
+// Binary Sorting algorithm written in JavaScript
+
+function binarySort(arr) {
+    let temp;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr;
+}
