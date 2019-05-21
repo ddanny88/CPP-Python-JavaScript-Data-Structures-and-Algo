e673959e7e24f8a397d65892ad518562897bdf81
Daniel Dominguez <ddominguez20.dd@gmail.com>
2019-05-20 19:56:20 -0500

    binary search tree js and py

---
+++ Js/binarySearchTree.js
@@ -0,0 +1,62 @@
+// Binary Search Tree Written in JavaScript.
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.val) {
+                    return true;
+                }
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    find(val) {
+        if (!this.root) return undefined;
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if (val < current.val) {
+                current = current.left;
+            } else if (val > current.val) {
+                current = current.right;
+            } else {
+                found = true;
+                return current;
+            }
+        }
+        if (!found) return false;
+    }
+}
---
+++ Python/binary_search_tree.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Binary_search_tree:
+    
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, val):
+        new_node = Node(val)
+        if not self.root:
+            self.root = new_node
+            return self
+        else:
+            current = self.root
+            while True:
+                if val == current.val:
+                    return True
+                if val < current.val:
+                    if current.left == None:
+                        current.left = new_node
+                        return self
+                    else:
+                        current = current.left
+                elif val > current.val:
+                    if current.right == None:
+                        current.right = new_node
+                    else:
+                        current = current.right
+
+    def find(self, val):
+        if not self.root:
+            return None
+        current = self.root
+        found = False
+        while current and not found:
+            if val < current.val: 
+                current = current.left
+            elif val > current.val:
+                current = current.right
+            else:
+                found = True
+                return current
+        if not found:
+            return False
